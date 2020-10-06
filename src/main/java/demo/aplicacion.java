6aea98512eb4b4f0b111f81bd456b9f736ac572e
Diego <diegortizv31@gmail.com>
2020-10-06 00:37:51 -0500

    Segundo commit

--- src/main/java/demo/aplicacion.java
+++ src/main/java/demo/aplicacion.java
@@ -5,7 +5,6 @@ public class aplicacion {
     public static void main(String[] args) {
         int a = 12;
         int b = 2;
-        System.out.println("Hola mundo");
         System.out.println(a+b);
     }
     

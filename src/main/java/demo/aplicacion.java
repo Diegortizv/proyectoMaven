e449bcf939e5737e858df4bfb5eb76810d2721be
Diego <diegortizv31@gmail.com>
2020-10-06 12:18:28 -0500

    Commit 3

--- src/main/java/demo/aplicacion.java
+++ src/main/java/demo/aplicacion.java
@@ -3,9 +3,9 @@ package demo;
 public class aplicacion {
     
     public static void main(String[] args) {
-        int a = 12;
-        int b = 2;
-        System.out.println(a+b);
+        String s = "Leo es un pendejo";
+        System.out.println(s);
+        
     }
     
 }
